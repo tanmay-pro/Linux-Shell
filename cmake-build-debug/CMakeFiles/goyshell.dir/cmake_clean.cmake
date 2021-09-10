b62c47907cc731fa24a74e242072473303363920
tanmay-pro <tanmay.goyal@students.iiit.ac.in>
2021-09-10 11:18:52 +0530

    Loaded Clion Project

---
+++ cmake-build-debug/CMakeFiles/goyshell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goyshell.dir/main.c.o"
+  "goyshell"
+  "goyshell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/goyshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/goyshell.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for goyshell.
+# This may be replaced when dependencies are built.
