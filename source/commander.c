d6eeaa787ad9a7367ef434470553ac14cf790c1c
tanmay-pro <tanmay.goyal@students.iiit.ac.in>
2021-09-25 09:18:36 +0530

    Indentation Refining

--- source/commander.c
+++ source/commander.c
@@ -5,36 +5,36 @@ int decide_command(char *str, int *process_num)
 {
 	char cpy[strlen(str) + 1];
 	strcpy(cpy, str);
-	char *token , *saveptr = NULL;
+	char *token, *saveptr = NULL;
 	token = strtok_r(cpy, " \t\n", &saveptr);
 
-	if(strcmp(token, "echo") == 0)
+	if (strcmp(token, "echo") == 0)
 	{
 		echo_func(str);
 	}
-	else if(strcmp(token, "pwd") == 0)
+	else if (strcmp(token, "pwd") == 0)
 	{
 		pwd_func();
 	}
-	else if(strcmp(token, "exit") == 0 || strcmp(token, "quit") == 0)
+	else if (strcmp(token, "exit") == 0 || strcmp(token, "quit") == 0)
 	{
 		signal(SIGCHLD, null_func);
 		zombie_handler();
 		return 0;
 	}
-	else if(strcmp(token, "cd") == 0)
+	else if (strcmp(token, "cd") == 0)
 	{
 		cd(str);
 	}
-	else if(strcmp(token, "ls") == 0)
+	else if (strcmp(token, "ls") == 0)
 	{
 		ls_decider(str);
 	}
-	else if(strcmp(token, "repeat") == 0)
+	else if (strcmp(token, "repeat") == 0)
 	{
 		repeat(str, process_num);
 	}
-	else if(strcmp(token, "pinfo") == 0)
+	else if (strcmp(token, "pinfo") == 0)
 	{
 		pinfo(str);
 	}
