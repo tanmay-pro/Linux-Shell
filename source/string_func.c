935617625de4ba90a68f9e7f70c7ff97ae00d922
tanmay-pro <tanmay.goyal@students.iiit.ac.in>
2021-09-26 15:33:23 +0530

    Corrected Echo Function

--- source/string_func.c
+++ source/string_func.c
@@ -29,28 +29,6 @@ char* substr(const char *src, int m, int n)
 	return dest;
 }
 
-char *remove_ws_middle(char *str)
-{
-	char *final = (char*)malloc(sizeof(char)* (strlen(str) + 1));
-	int pos = 0;
-	final[pos] = str[0];
-	pos++;
-	for(int i = 1; i< strlen(str); i++)
-	{
-		if(str[i] == ' ' && str[i-1]== ' ')
-		{
-			;
-		}
-		else
-		{
-			final[pos] = str[i];
-			pos++;
-		}
-	}
-	final[pos] = '\0';
-	return final;
-}
-
 int x_part_of_y(char *str1, char *str2)
 {
 	int set = 0;
